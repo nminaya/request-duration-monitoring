405861311cf027a11a1f59db5946503916d0c984
Nelson Minaya <nelson_minaya@claro.com.do>
2018-03-14 09:38:49 -0400

    First commit

---
+++ Metrics.Library/MetricHttpClientHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Metrics.Library
+{
+	public class MetricHttpClientHandler : HttpClientHandler
+	{
+		protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			bool successful = false;
+			var stopwatch = Stopwatch.StartNew();
+
+			try
+			{
+				var result = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+				successful = result.IsSuccessStatusCode;
+
+				return result;
+			}
+			finally
+			{
+				stopwatch.Stop();
+			}
+		}
+	}
+}
